2c88289b9dc7418a6866f3faa8b2815cec660b19
wearrier <a.wearrier@icloud.com>
2026-02-20 18:43:00 +0900

    Initial Commit

---
+++ musicPlayer/musicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicPlayerApp.swift
+//  musicPlayer
+//
+//  Created by wearrier on 2026/02/20.
+//
+
+import SwiftUI
+
+@main
+struct musicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

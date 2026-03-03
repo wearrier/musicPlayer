64b026504fa011c171724f809e2e6f8d21db0eb9
wearrier <a.wearrier@icloud.com>
2026-03-03 20:07:24 +0900

    first commit

---
+++ musicPlayer/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  musicPlayer
+//
+//  Created by wearrier on 2026/02/20.
+//
+
+import SwiftUI
+
+struct Player: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Player()
+}
---
+++ musicPlayer/musicPlayer.swift
@@ -0,0 +1,153 @@
+//
+//  musicPlayer.swift
+//  musicPlayer
+//
+//  Created by wearrier on 2026/02/24.
+//
+
+import SwiftUI
+import Foundation
+import AVFoundation
+internal import Combine
+
+internal final class musicPlayer: ObservableObject
+{
+    //プレイヤー
+    @Published var player: AVAudioPlayer?
+    //ファイルパス
+    @Published var url: String
+    @Published var listOfName: String
+    //タイマー
+    @Published var timer: Timer?
+    @Published var elapsedSeconds: Double
+    @Published var durationTime: Double
+    //ファイルのリスト
+    @Published var listMucsic = Set<UUID>()
+    @Published var fileList: [String]
+    @Published var index: Int
+    
+    //ランダム要素オン／オフ
+    @Published var isRandom: Bool = false
+    
+    //ループ設定
+    @Published var isLoop: Bool = false
+    
+    @Published var isEditing: Bool = false
+    
+    init(player: AVAudioPlayer? = nil, url: String = "", listOfName: String = "", timer: Timer? = nil, elapsedSeconds: Double, durationTime: Double, fileList: [String], index: Int) {
+        self.player = player
+        self.url = url
+        self.listOfName = listOfName
+        self.timer = timer
+        self.elapsedSeconds = elapsedSeconds
+        self.durationTime = durationTime
+        self.fileList = fileList
+        self.index = index
+    }
+    //再生処理
+    func play()
+    {
+        //プレイリストが存在しない場合はエラー
+        if fileList.isEmpty == true
+        {
+            print ("ファイルがありません")
+            return
+        }
+        
+        //プレイリストが存在していれば再生
+        else
+        {
+            
+            if(isRandom == true)
+            {
+                index = Int.random(in: 0..<fileList.count)
+            }
+            
+            listOfName = url + "/" + fileList[index]
+            
+            //再生処理
+            let playerURL = URL(fileURLWithPath: listOfName)
+            do
+            {
+                player?.stop()
+                print(listOfName)
+                player = try AVAudioPlayer(contentsOf: playerURL)
+                Loop()
+                if(player?.isPlaying == false)
+                {
+                    player?.prepareToPlay()
+                    player?.play()
+                    player?.currentTime = 0
+                    //ファイルの最終端を取得
+                    durationTime = player!.duration
+                }
+            }
+            catch
+            {
+                print ("Error loading audio file[Error: \(error)]")
+            }
+        }
+        //再生時間取得
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true)
+        {
+            _ in
+            self.elapsedSeconds = self.player!.currentTime
+        }
+    }
+    
+    //停止処理
+    func stop()
+    {
+        player?.stop()
+        player?.currentTime = 0.0
+    }
+    
+    //プレイリスト生成
+    func playlistAdd()
+    {
+        let fileManager = FileManager.default
+        
+        do
+        {
+            self.fileList = try fileManager.contentsOfDirectory(atPath: url)
+            print("\(self.fileList)")
+        }
+        catch
+        {
+            print("Error retrieving files from directory.")
+            return
+        }
+    }
+    
+    //ループ
+    func Loop()
+    {
+        player?.numberOfLoops = isLoop ? -1 : 0
+    }
+    //次
+    func nextPlay()
+    {
+        if(index == fileList.endIndex - 1)
+        {
+            index = fileList.startIndex - 1
+        }
+        
+        if(index >= fileList.startIndex - 1)
+        {
+            index += 1
+        }
+    }
+    //戻る
+    func backPlay()
+    {
+        if(index == fileList.startIndex)
+        {
+            index = fileList.endIndex
+        }
+        
+        if(index <= fileList.endIndex)
+        {
+            index -= 1
+        }
+    }
+}
